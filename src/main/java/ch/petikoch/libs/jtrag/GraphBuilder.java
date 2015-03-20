6e17d30dae17dcdfd765d44a889b8061ea4c98e7
Peti Koch <petikoch@gmail.com>
2015-03-20 16:24:29 +0100

    added license plugin (thanks to @aalmiray for pointing me to it)

--- src/main/java/ch/petikoch/libs/jtrag/GraphBuilder.java
+++ src/main/java/ch/petikoch/libs/jtrag/GraphBuilder.java
@@ -1,4 +1,4 @@
-/*
+/**
  * Copyright 2015 Peti Koch
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package ch.petikoch.libs.jtrag;
 
 import ch.petikoch.libs.jtwfg.Graph;
