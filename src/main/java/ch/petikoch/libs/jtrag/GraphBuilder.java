3b77895e58e40810eae3e8f5a5d8eca073bb9121
peti <petikoch@gmail.com>
2021-04-05 09:31:39 +0200

    Various updates (Java 7 --> 8, Gradle Wrapper 3.1 --> 6.8.3, ...) + use Maven Central instead of Bintray/JCenter
    * Features
      * No new features
    * Bugfixes
      * No bug fixes
    * Changes
      * Minimum Java version 8 (instead of 7)
      * Update gradle wrapper to 6.8.2 (from 3.1)
      * Use Groovy 2.5.x in the tests (instead of 2.4.x)
      * Publish to Maven Central instead Bintray/JCenter (Bintray/JCenter will be shut down soon)
      * Simplify build setup: remove Jacoco and Coveralls

--- src/main/java/ch/petikoch/libs/jtrag/GraphBuilder.java
+++ src/main/java/ch/petikoch/libs/jtrag/GraphBuilder.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2015 Peti Koch
+ * Copyright 2021 Peti Koch
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,104 +20,102 @@ import ch.petikoch.libs.jtwfg.Graph;
 /**
  * A builder class for graph instance creation. Can be used concurrently by different threads which create together the
  * graph.
- * <p/>
+ * <p>
  * Thread-safe.
  *
- * @param <T>
- * 		The type of the ID of the tasks. Something with a meaningful {@link Object#equals(Object)} and {@link
- * 		Object#hashCode()} implementation like {@link String}, {@link Long} or a class of your domain model which is fine
- * 		to use as a key e.g. in a {@link java.util.HashMap}. If T implements Comparable, then you get sorted collections.
- * @param <R>
- * 		The type of the ID of the resources. Something with a meaningful {@link Object#equals(Object)} and {@link
- * 		Object#hashCode()} implementation like {@link String}, {@link Long} or a class of your domain model which is fine
- * 		to use as a key e.g. in a {@link java.util.HashMap}. If T implements Comparable, then you get sorted collections.
+ * @param <T> The type of the ID of the tasks. Something with a meaningful {@link Object#equals(Object)} and {@link
+ *            Object#hashCode()} implementation like {@link String}, {@link Long} or a class of your domain model which is fine
+ *            to use as a key e.g. in a {@link java.util.HashMap}. If T implements Comparable, then you get sorted collections.
+ * @param <R> The type of the ID of the resources. Something with a meaningful {@link Object#equals(Object)} and {@link
+ *            Object#hashCode()} implementation like {@link String}, {@link Long} or a class of your domain model which is fine
+ *            to use as a key e.g. in a {@link java.util.HashMap}. If T implements Comparable, then you get sorted collections.
  */
 public class GraphBuilder<T, R> {
 
-	private final ch.petikoch.libs.jtwfg.GraphBuilder<Object> jtwfgGraphBuilderDelegate;
+    private final ch.petikoch.libs.jtwfg.GraphBuilder<Object> jtwfgGraphBuilderDelegate;
 
-	public GraphBuilder() {
-		this(new ch.petikoch.libs.jtwfg.GraphBuilder<>());
-	}
+    public GraphBuilder() {
+        this(new ch.petikoch.libs.jtwfg.GraphBuilder<>());
+    }
 
-	private GraphBuilder(final ch.petikoch.libs.jtwfg.GraphBuilder<Object> jtwfgGraphBuilderDelegate) {
-		this.jtwfgGraphBuilderDelegate = jtwfgGraphBuilderDelegate;
-	}
+    private GraphBuilder(final ch.petikoch.libs.jtwfg.GraphBuilder<Object> jtwfgGraphBuilderDelegate) {
+        this.jtwfgGraphBuilderDelegate = jtwfgGraphBuilderDelegate;
+    }
 
-	public GraphBuilder<T, R> addTask(T taskId) {
-		jtwfgGraphBuilderDelegate.addTask(taskId);
-		return this;
-	}
+    public GraphBuilder<T, R> addTask(T taskId) {
+        jtwfgGraphBuilderDelegate.addTask(taskId);
+        return this;
+    }
 
-	public GraphBuilder<T, R> addTasks(Iterable<T> taskIds) {
-		//noinspection unchecked
-		jtwfgGraphBuilderDelegate.addTasks((Iterable<Object>) taskIds);
-		return this;
-	}
+    public GraphBuilder<T, R> addTasks(Iterable<T> taskIds) {
+        //noinspection unchecked
+        jtwfgGraphBuilderDelegate.addTasks((Iterable<Object>) taskIds);
+        return this;
+    }
 
-	public boolean hasTask(T taskId) {
-		return jtwfgGraphBuilderDelegate.hasTask(taskId);
-	}
+    public boolean hasTask(T taskId) {
+        return jtwfgGraphBuilderDelegate.hasTask(taskId);
+    }
 
-	public GraphBuilder<T, R> removeTask(T taskId) {
-		jtwfgGraphBuilderDelegate.removeTask(taskId);
-		return this;
-	}
+    public GraphBuilder<T, R> removeTask(T taskId) {
+        jtwfgGraphBuilderDelegate.removeTask(taskId);
+        return this;
+    }
 
-	public GraphBuilder<T, R> removeTasks(Iterable<T> taskIds) {
-		//noinspection unchecked
-		jtwfgGraphBuilderDelegate.removeTasks((Iterable<Object>) taskIds);
-		return this;
-	}
+    public GraphBuilder<T, R> removeTasks(Iterable<T> taskIds) {
+        //noinspection unchecked
+        jtwfgGraphBuilderDelegate.removeTasks((Iterable<Object>) taskIds);
+        return this;
+    }
 
-	public GraphBuilder<T, R> addResource(R resourceId) {
-		jtwfgGraphBuilderDelegate.addTask(resourceId);
-		return this;
-	}
+    public GraphBuilder<T, R> addResource(R resourceId) {
+        jtwfgGraphBuilderDelegate.addTask(resourceId);
+        return this;
+    }
 
-	public GraphBuilder<T, R> addResources(Iterable<R> resourceIds) {
-		//noinspection unchecked
-		jtwfgGraphBuilderDelegate.addTasks((Iterable<Object>) resourceIds);
-		return this;
-	}
+    public GraphBuilder<T, R> addResources(Iterable<R> resourceIds) {
+        //noinspection unchecked
+        jtwfgGraphBuilderDelegate.addTasks((Iterable<Object>) resourceIds);
+        return this;
+    }
 
-	public boolean hasResource(R resourceId) {
-		return jtwfgGraphBuilderDelegate.hasTask(resourceId);
-	}
+    public boolean hasResource(R resourceId) {
+        return jtwfgGraphBuilderDelegate.hasTask(resourceId);
+    }
 
-	public GraphBuilder<T, R> removeResource(R resourceId) {
-		jtwfgGraphBuilderDelegate.removeTask(resourceId);
-		return this;
-	}
+    public GraphBuilder<T, R> removeResource(R resourceId) {
+        jtwfgGraphBuilderDelegate.removeTask(resourceId);
+        return this;
+    }
 
-	public GraphBuilder<T, R> removeResources(Iterable<R> resourceIds) {
-		//noinspection unchecked
-		jtwfgGraphBuilderDelegate.removeTasks((Iterable<Object>) resourceIds);
-		return this;
-	}
+    public GraphBuilder<T, R> removeResources(Iterable<R> resourceIds) {
+        //noinspection unchecked
+        jtwfgGraphBuilderDelegate.removeTasks((Iterable<Object>) resourceIds);
+        return this;
+    }
 
-	public GraphBuilder<T, R> addTask2ResourceDependency(T taskId, R resourceId) {
-		jtwfgGraphBuilderDelegate.addTaskWaitsFor(taskId, resourceId);
-		return this;
-	}
+    public GraphBuilder<T, R> addTask2ResourceDependency(T taskId, R resourceId) {
+        jtwfgGraphBuilderDelegate.addTaskWaitsFor(taskId, resourceId);
+        return this;
+    }
 
-	public GraphBuilder<T, R> removeTask2ResourceDependency(T taskId, R resourceId) {
-		jtwfgGraphBuilderDelegate.removeTaskWaitForDependency(taskId, resourceId);
-		return this;
-	}
+    public GraphBuilder<T, R> removeTask2ResourceDependency(T taskId, R resourceId) {
+        jtwfgGraphBuilderDelegate.removeTaskWaitForDependency(taskId, resourceId);
+        return this;
+    }
 
-	public GraphBuilder<T, R> addResource2TaskAssignment(R resourceId, T taskId) {
-		jtwfgGraphBuilderDelegate.addTaskWaitsFor(resourceId, taskId);
-		return this;
-	}
+    public GraphBuilder<T, R> addResource2TaskAssignment(R resourceId, T taskId) {
+        jtwfgGraphBuilderDelegate.addTaskWaitsFor(resourceId, taskId);
+        return this;
+    }
 
-	public GraphBuilder<T, R> removeResource2TaskAssignment(R resourceId, T taskId) {
-		jtwfgGraphBuilderDelegate.removeTaskWaitForDependency(resourceId, taskId);
-		return this;
-	}
+    public GraphBuilder<T, R> removeResource2TaskAssignment(R resourceId, T taskId) {
+        jtwfgGraphBuilderDelegate.removeTaskWaitForDependency(resourceId, taskId);
+        return this;
+    }
 
-	public Graph<Object> build() {
-		return jtwfgGraphBuilderDelegate.build();
-	}
+    public Graph<Object> build() {
+        return jtwfgGraphBuilderDelegate.build();
+    }
 
 }
